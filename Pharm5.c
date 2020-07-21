4923a15f97c6d9bd36ed789c525045b35aa3dae7
matt <matt_netter@hotmail.com>
2020-07-20 20:07:03 -0400

    july 20 character to strings

--- Pharm5.c
+++ Pharm5.c
@@ -3,11 +3,14 @@
 #include <string.h>
 #include <time.h>
 
-char array[]= {'A', 'E', 'I', 'O', 'U', 'Y', 'B', 'X', 'K', 'L', 'P'};
+//char array[]= {'A', 'E', 'I', 'O', 'U', 'Y', 'B', 'X', 'K', 'L', 'P', '\0'};
 
+char *arrayOfStrings[] = {"AE", "IE", "EE", "OO", "OY", "PH", "TT", "BL", "KR"};
 
 
 int main(int argc, char *argv[]){
+
+   
 
 int userArg =0;
 int userArg2 =0;
@@ -21,16 +24,17 @@ userArg2 = atoi(argv[2]);
 		for(int i=0; i<userArg; i++){
 	
 			if(i % 2 ==0)
-			printf("%c", array[rand() % 5]);
+			printf("%s", arrayOfStrings[rand() % 5]);
 			else {
 
 		int n = rand()%(10-5)+ 5;
-		printf("%c", array[n]);
+		printf("%s", arrayOfStrings[n]);
 		}
 		}
 		printf("\n");
 	
 	}
+	
 
 	return 0;
 	
